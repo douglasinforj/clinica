0910e6146c1a93f7530b1a8788791e20e5794d23
Douglas Silva <douglasitpro@gmail.com>
2024-06-24 09:44:16 -0300

    criando e Configurando o arquivo urls.py com as rotas do APP Cadastro

---
+++ cadastros/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('clientes/', views.cliente_list, name='cliente_list'),
+    path('clientes/novo/', views.cliente_create, name='cliente_create'),
+    path('clientes/<int:id>/editar/', views.cliente_update, name='cliente_update'),
+    path('clientes/<int:id>/deletar/', views.cliente_delete, name='cliente_delete'),
+    path('clientes/<int:id>/exames/', views.exame_list, name='exame_list'),
+    path('clientes/<int:id>/exames/novo/', views.exame_create, name='exame_create'),
+    path('clientes/<int:cliente_id>/exames/<int:id>/editar/', views.exame_update, name='exame_update'),
+    path('clientes/<int:cliente_id>/exames/<int:id>/deletar/', views.exame_delete, name='exame_delete'),
+]
