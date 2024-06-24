dc249a41ed68d8fa299fa6f654aecc94f048554f
Douglas Silva <douglasitpro@gmail.com>
2024-06-24 08:15:27 -0300

    models Cliente

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Cliente(models.Model):
+    nome = models.models.CharField(max_length=50)
+    cpf = models.CharField(max_length=14, unique=True)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    data_nascimento = models.DateField(blank=True, null=True)
 
+    def __str__(self):
+        return self.nome
