3d53d3df95cafd2a7db21358f515f6570e8efeb3
Douglas Silva <douglasitpro@gmail.com>
2024-06-24 19:44:37 -0300

    Adicionando class CSS diretamento no form Cliente e Exame

--- cadastros/forms.py
+++ cadastros/forms.py
@@ -4,9 +4,25 @@ from .models import Cliente, Exame
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = ['nome', 'cpf', 'telefone', 'email', 'data_nascimento']
+        fields = '__all__'
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'cpf': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'data_nascimento': forms.TextInput(attrs={'class': 'form-control'}),
+            # Pode definir classes CSS para os campos aqui
+        }
 
 class ExameForm(forms.ModelForm):
     class Meta:
         model = Exame
-        fields = ['tipo_exame', 'data_exame', 'resultado', 'preco']
+        fields = '__all__'
+        widgets = {
+            'cliente': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipo_exame': forms.TextInput(attrs={'class': 'form-control'}),
+            'data_exame': forms.TextInput(attrs={'class': 'form-control'}),
+            'resultado': forms.Textarea(attrs={'class': 'form-control'}),
+            'preco': forms.TextInput(attrs={'class': 'form-control'}),
+            # Pode definir classes CSS para os campos aqui
+        }
