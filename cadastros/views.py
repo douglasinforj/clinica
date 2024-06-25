45d55d21a9ecabbe878f4c8c6670cc5f872e907b
Douglas Silva <douglasitpro@gmail.com>
2024-06-24 21:17:54 -0300

    Restringindo acesso as views, só pondendo usuário autenticados

--- cadastros/views.py
+++ cadastros/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cliente, Exame
 from .forms import ClienteForm, ExameForm
 from django.db.models import Sum, Q
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     return render(request, 'cadastros/home.html')
 
 
-
+@login_required
 def cliente_list(request):
     termo_pesquisa = request.GET.get('q','')
 
@@ -20,12 +22,12 @@ def cliente_list(request):
 
 
 
-
+@login_required
 def cliente_detail(request, id):
     clientes = get_object_or_404(Cliente, id=id)
     return render(request, 'cadastros/cliente_detail.html', {'clientes': clientes})
 
-
+@login_required
 def cliente_create(request):
     if request.method == "POST":
         form = ClienteForm(request.POST, request.FILES)     #adicionando foto
@@ -36,6 +38,8 @@ def cliente_create(request):
         form = ClienteForm()
     return render(request, 'cadastros/cliente_form.html', {'form': form})
 
+
+@login_required
 def cliente_update(request, id):
     clientes = get_object_or_404(Cliente, id=id)
     if request.method == "POST":
@@ -47,19 +51,25 @@ def cliente_update(request, id):
         form = ClienteForm(instance=clientes)
     return render(request, 'cadastros/cliente_form.html', {'form': form})
 
+
+@login_required
 def cliente_delete(request, id):
     clientes = get_object_or_404(Cliente, id=id)
     if request.method == "POST":
         clientes.delete()
         return redirect('cliente_list')
-    return render(request, 'cadastros/cliente_confirm_delete.html', {'cliente':clientes})
+    return render(request, 'cadastros/cliente_confirm_delete.html', {'clientes':clientes})
 
+
+@login_required
 def exame_list(request, id):
     cliente = get_object_or_404(Cliente, id=id)
     exames = Exame.objects.filter(cliente=cliente)
     total_preco = exames.aggregate(Sum('preco'))['preco__sum'] or 0.00  # Calculando o total dos preços
     return render(request, 'cadastros/exame_list.html', {'exames': exames, 'cliente': cliente, 'total_preco': total_preco})
-          
+
+
+@login_required
 def exame_create(request, id):
     cliente = get_object_or_404(Cliente, id=id)
     if request.method == "POST":
@@ -73,6 +83,8 @@ def exame_create(request, id):
         form = ExameForm()
     return render(request, 'cadastros/exame_form.html', {'form': form, 'cliente': cliente})  
 
+
+@login_required
 def exame_update(request, cliente_id, id):
     exame = get_object_or_404(Exame, id=id, cliente_id=cliente_id)
     if request.method == "POST":
@@ -84,6 +96,8 @@ def exame_update(request, cliente_id, id):
         form = ExameForm(instance=exame)
     return render(request, 'cadastros/exame_form.html', {'form': form, 'cliente': exame.cliente})
 
+
+@login_required
 def exame_delete(request, cliente_id, id):
     exame = get_object_or_404(Exame, id=id, cliente_id=cliente_id)
     if request.method == "POST":
